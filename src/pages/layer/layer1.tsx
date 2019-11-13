a8d48372bdff7fe4d94dddf62d039ff355fccdfe
xiaoping.xu <xiaoping.xu@weimob.com>
2019-11-13 16:51:47 +0800

    f

---
+++ src/pages/layer/layer1.tsx
@@ -0,0 +1,27 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+
+export default class PageView extends Component {
+  constructor() {
+    super(...arguments)
+  }
+
+  render() {
+    return (
+      <View className='components-page'>
+        <Text>flex-direction: row 横向布局</Text>
+        <View className='flex-wrp' style='flex-direction:row;'>
+          <View className='flex-item demo-text-1'/>
+          <View className='flex-item demo-text-2'/>
+          <View className='flex-item demo-text-3'/>
+        </View>
+        <Text>flex-direction: column 纵向布局</Text>
+        <View className='flex-wrp' style='flex-direction:column;'>
+          <View className='flex-item flex-item-V demo-text-1'/>
+          <View className='flex-item flex-item-V demo-text-2'/>
+          <View className='flex-item flex-item-V demo-text-3'/>
+        </View>
+      </View>
+    )
+  }
+}
